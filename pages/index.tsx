604d89558a3c6195a04cebf9ac745f78697bc173
ronfl3x <jupitr.gabriel@gmail.com>
2023-01-01 10:18:34 +0200

    first

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  return (
+    <div className="relative flex min-h-screen">
+      <Head>
+        <title>Jupido</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className=""></main>
+    </div>
+  );
+};
+
+export default Home;
